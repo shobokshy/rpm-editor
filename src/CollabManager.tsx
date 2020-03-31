c9da0a0aa4053c56fc99a2f0b20493ba273d3675
shobokshy <a.shobokshy@gmail.com>
2020-03-31 15:36:07 +1000

    Improved Collab API for better reliability on slow connections

--- src/CollabManager.tsx
+++ src/CollabManager.tsx
@@ -8,9 +8,14 @@ import { Schema } from 'prosemirror-model';
 import throttle = require('lodash.throttle');
 
 export interface CollabManagerConfig {
-    onIncomingSteps: (cb: () => void) => void;
-    onSendableSteps: (id: string | number, sendableSteps: { version: number, steps: Step < any > [], clientId: string | number, origins?: Transaction < any > [] }) => void;
+    onIncomingSteps: (cb: (steps?: StepsInfo) => void) => void;
+    onSendableSteps: (
+        id: string | number, 
+        sendableSteps: { version: number, steps: Step < any > [], clientId: string | number, origins?: Transaction < any > [] },
+        acknowledged: (steps: StepsInfo) => void
+    ) => void;
     getNewSteps: (id: string | number, version: number, schema: Schema, cb: (steps: StepsInfo) => void) => void;
+    onConnectionDrop: (cb: () => void) => void;
 }
 
 interface CollabManagerComponentProps extends EditorContext {
@@ -21,6 +26,8 @@ interface CollabManagerComponentProps extends EditorContext {
 const CollabManagerComponent: React.FC<CollabManagerComponentProps> = (props) => {
 
     const editorState = React.useRef<EditorState>(props.editorState);
+    const savedVersion = React.useRef<number>(-1);
+    const waitingForAck = React.useRef<boolean>(false);
     const sendThrottled = React.useRef(throttle(() => {
             sendSteps()
         }, props.throttleWait || 1000)
@@ -34,53 +41,57 @@ const CollabManagerComponent: React.FC<CollabManagerComponentProps> = (props) =>
         }
     }, [])
 
-    React.useEffect(() => {
-        window.onbeforeunload = () => {
-            getSendableSteps(true);
-            const sendable = collab.sendableSteps(editorState.current);
+    // React.useEffect(() => {
+    //     window.onbeforeunload = () => {
+    //         getSendableSteps(true);
+    //         const sendable = collab.sendableSteps(editorState.current);
+    //         console.log(sendable);
+    //         if (sendable && sendable.steps.length > 0) return "You have unsaved content! Are you sure you want to leave?";
+    //     }
 
-            if (sendable && sendable.steps.length > 0) return "You have unsaved content! Are you sure you want to leave?";
-        }
-
-        return () => {
-            window.onbeforeunload = null;
-        }
-    }, [])
-
-    React.useEffect(() => {
-        getSendableSteps();
-    }, [props.editorState])
+    //     return () => {
+    //         window.onbeforeunload = null;
+    //     }
+    // }, [])
 
     React.useLayoutEffect(() => {
         editorState.current = props.editorState;
-    })
+        getSendableSteps(false);
+    }, [props.editorState.doc]);
 
     const listenForSteps = () => {
-        let requestedVersion = -1;
 
-        props.config.onIncomingSteps(() => {
+        props.config.onIncomingSteps((incoming) => {
             const currentVersion = collab.getVersion(editorState.current);
 
-            if(currentVersion > requestedVersion) {
-                requestedVersion = currentVersion;
-
+            if (incoming) {
+                receiveTransaction(incoming);
+            } else {
                 props.config.getNewSteps(props.id, currentVersion, props.editorState.schema, (incomingSteps) => {
-                    props.dispatchTransaction(
-                        collab.receiveTransaction(editorState.current, incomingSteps.steps, incomingSteps.clientIds)
-                    )
+                    receiveTransaction(incomingSteps);
                 })
             }
-        })
+        });
+
+        props.config.onConnectionDrop(() => {
+            waitingForAck.current = false;
+        });
     }
 
     const sendSteps = () => {
         const sendable = collab.sendableSteps(editorState.current);
 
-        if (sendable) props.config.onSendableSteps(props.id, {
-            version: sendable.version,
-            clientId: sendable.clientID,
-            steps: sendable.steps
-        });
+        if (sendable && !waitingForAck.current) {
+            waitingForAck.current = true;
+            props.config.onSendableSteps(props.id, {
+                version: sendable.version,
+                clientId: sendable.clientID,
+                steps: sendable.steps
+            }, (acknowledged: StepsInfo) => {
+                receiveTransaction(acknowledged);
+                waitingForAck.current = false;
+            });
+        }
     }
 
     const getSendableSteps = (unthrottled?: boolean) => {
@@ -91,6 +102,18 @@ const CollabManagerComponent: React.FC<CollabManagerComponentProps> = (props) =>
         }
     }
 
+    const receiveTransaction = (data: StepsInfo) => {
+        if (data.version! > savedVersion.current) {
+            props.dispatchTransaction(
+                collab.receiveTransaction(
+                    editorState.current,
+                    data.steps,
+                    data.clientIds
+                )
+            )
+            savedVersion.current = data.version!;
+        }
+    }
 
     return null;
 }
